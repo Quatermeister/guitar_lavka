94f0c21a988cee7269ee968c41f6aa2dddcce22d
Quatermeister <Quersten@yandex.ru>
2022-04-15 15:02:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".header_slider_container").slick({
+		speed: 800,
+		autoplay:true,
+		autoplaySpeed:3500,
+		appendArrows:$(".header_slider_container")
+	});
+});
